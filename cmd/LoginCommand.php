8f9bb40495e456eee896e83bac8b18bd84bd05cf
ship <543999860@qq.com>
2020-07-22 16:30:32 +0800

    login

--- cmd/LoginCommand.php
+++ cmd/LoginCommand.php
@@ -8,8 +8,8 @@
 
 namespace Cmd;
 
-use GuzzleHttp\Psr7\Request;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,15 +18,19 @@ class LoginCommand extends Command
     protected function configure()
     {
         $this->setName('user:login')
+            // 配置一个参数
+            ->addArgument('name', InputArgument::REQUIRED, '账户')
             ->setDescription('用户登录');
     }
     protected function interact(InputInterface $input, OutputInterface $output)
     {
-
+        $fd = STDIN;
+        var_dump(fgets($fd));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output){
         try{
+            var_dump($input->getArgument('name'));
             if(BBClient::instance()->login('13716526885')){
                 $output->write('登录成功');
             }
