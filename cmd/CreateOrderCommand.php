12327e655d04e5e003b2f9d8f728b505b52ef755
ship <543999860@qq.com>
2020-07-24 13:09:54 +0800

    login

--- cmd/CreateOrderCommand.php
+++ cmd/CreateOrderCommand.php
@@ -8,8 +8,8 @@
 
 namespace Cmd;
 use Cmd\service\CreateOrder;
-use GuzzleHttp\Psr7\Request;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,16 +18,20 @@ class CreateOrderCommand extends Command
     protected function configure()
     {
         $this
-            // 命令的名称 （"php console_command" 后面的部分）
             ->setName('order:create')
-            // 运行 "php console_command list" 时的简短描述
-            ->setDescription('活动店铺列表');
+            ->setDescription('创建订单')
+            ->addArgument('u',InputArgument::REQUIRED,'手机号')
+            ->addArgument('g',InputArgument::REQUIRED,'商品id')
+            ->addArgument('a',InputArgument::REQUIRED,'活动id')
+            ->addArgument('d',InputArgument::REQUIRED,'配送地址id')
+            ->addArgument('s',InputArgument::OPTIONAL,'店铺id',1)
+            ->addArgument('z',InputArgument::OPTIONAL,'自取手机号','');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if(BBClient::instance()->login('13716526885')){
-            $order = new CreateOrder(66257,1089,1,77,'1484644546');
+        if(BBClient::instance()->login($input->getArgument('u'))){
+            $order = new CreateOrder($input->getArgument('g'),$input->getArgument('d'),$input->getArgument('s'),$input->getArgument('a'),$input->getArgument('z'));
             $response = $order->run();
             var_dump($response);exit;
         }
